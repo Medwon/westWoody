2cf39136b4d9a4d82617b1441583306ae23c2c2c
baglan <baglansagidulladev@gmail.com>
2026-01-18 15:58:31 +0100

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.westwood.app/api/v1',
+  apiUrl: 'https://ww-reward-backend-401aa2c307ef.herokuapp.com/api/v1',
   appName: 'WestWood'
 };
 
